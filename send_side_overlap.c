52bdf9bda6afa5dfb74641be5573a20b6bcbba19
sramesh <sramesh@head.ri2.cse.ohio-state.edu>
2018-01-30 15:31:01 -0500

    Add theory behind optimization

--- send_side_overlap.c
+++ send_side_overlap.c
@@ -1,3 +1,17 @@
+/* Theory: By default, MVAPICH2 uses RDMA write for point-to-point data transfer using the rendezvous protocol.
+ * However, when it happens that the sending side uses non-blocking semantics and the receiving side uses 
+ * blocking semantics, this can be sub-optimal overall. Consider the following scenario:
+ * a. Sending side sends and RNDV_START and moves ahead with computation
+ * b. Receiving side is blocking, so it recieves the RNDV_START, and sends an RNDV_REPLY okaying the data transfer
+ * c. Since this is an RDMA_WRITE for the sending side, actual data transfer may not begin until the sending side reaches
+ *    MPI_Wait(), since the sending side needs to be "inside" MPI in order for communications to progress in the absence of a separate thread
+ * d. Thus, data transfer starts only when the sending side reaches MPI_Wait(), and this is serializing communication and computation from the sending
+ *    viewpoint.
+ * e. In such a situation of non-blocking sender and blocking receiver without a separate thread, it is easy to show that using an RDMA_READ from the receiving side is
+ *    optimal or near-optimal. Essentially we take advantage of the fact that the receiver has "nothing" to do when blocking on a receive, and might as well progress communication
+ *
+ *    In order to see this behaviour, set MV2_RNDV_PROTOCOL=RGET (default is RPUT), and see the reduction in overall runtime. This is attributed to better sender side overlap.
+ */
 #include<stdio.h>
 #include<mpi.h>
 
